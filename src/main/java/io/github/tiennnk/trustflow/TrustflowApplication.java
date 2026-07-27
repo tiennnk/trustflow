6ce3f6aa4e136598b85c41fc82121572f63ba64e
tiennnk <kieutiennguyenngoc2002.tn@gmail.com>
2026-07-28 00:15:12 +0700

    [chore] init Spring Boot project

---
+++ src/main/java/io/github/tiennnk/trustflow/TrustflowApplication.java
@@ -0,0 +1,13 @@
+package io.github.tiennnk.trustflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tiennnk/trustflow/TrustflowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tiennnk.trustflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
